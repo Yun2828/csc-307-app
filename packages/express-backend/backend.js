98931587ca7fde2e2b1c4a3da2d1a1b3524da35f
Yun <yunwaddyoo01@gmail.com>
2026-04-13 00:12:17 -0700

    getbyname

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -7,49 +7,63 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-// setup express to process data in JSON format
 app.use(express.json());
 
+const users = {
+    users_list: [
+    {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+    },
+    {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+    },
+    {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+    },
+    {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+    },
+    {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+    }
+]
+};
+app.get ("/", (req,res) => {
+    res.send("Hello World!");
+}) ;
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
 // API endpoint
 // accept http get request
-// '/' is the root path of the server, that will map to this endpoint
+// '/' is the root path of the server (localhost), that will map to this endpoint
 // receive a request and send a response
 app.get("/users", (req, res) => {
-  res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+} else {
+    res.send(users);
+}
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+    console.log(
+        `Example app listening at http://localhost:${port}`
+    );
 });
+console.log("Server is done...");
